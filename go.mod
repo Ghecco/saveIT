9d43d87d0e0d529ebda6288cda4af35a58d8ed1f
Francesco Pagano <64371507+Ghecco@users.noreply.github.com>
2021-07-08 00:45:46 +0200

    environment variables system

---
+++ go.mod
@@ -0,0 +1,8 @@
+module github.com/Ghecco/saveIT
+
+go 1.16
+
+require (
+	github.com/ichtrojan/thoth v1.3.0 // indirect
+	github.com/joho/godotenv v1.3.0 // indirect
+)
---
+++ main.go
@@ -0,0 +1,38 @@
+package main
+
+import (
+	"errors"
+	"log"
+	"os"
+
+	"github.com/ichtrojan/thoth"
+	"github.com/joho/godotenv"
+)
+
+func main() {
+	logger, err := thoth.Init("json")
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	if err := godotenv.Load(); err != nil {
+		logger.Log(errors.New(".env is not found"))
+		log.Fatal(".env is not found.")
+	}
+
+	version, versionExist := os.LookupEnv("VERSION")
+
+	if !versionExist {
+		logger.Log(errors.New("VERSION not set in .env"))
+		log.Fatal("PORT not set in .env")
+	}
+
+	port, exist := os.LookupEnv("PORT")
+
+	if !exist {
+		logger.Log(errors.New("PORT not set in .env"))
+		log.Fatal("PORT not set in .env")
+	}
+	log.Printf("Go_Todo App Version:%s ", version)
+	log.Print("Started web app on port :" + port)
+}
